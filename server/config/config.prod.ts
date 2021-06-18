40fd2791864188b8cb832e545bc77174da42d9cb
dimple-smile <1696819016@qq.com>
2021-06-18 15:20:55 +0800

    fix: prod config

---
+++ server/config/config.prod.ts
@@ -0,0 +1,10 @@
+import { EggAppConfig, PowerPartial } from 'egg'
+
+export default () => {
+  const config: PowerPartial<EggAppConfig> = {}
+  config.env = 'prod'
+  config.rundir = '/temp'
+  config.logger = { dir: '/tmp' }
+
+  return config
+}
